7907161540246a66b4f3315c0345f3b8ff924031
cms030303a-eng <cms030303a@gmail.com>
2025-11-15 17:47:04 +0900

    증가/감소/초기화 성공
    채터링 방지(디바운스 회로)만 넣으면 성공

--- led.v
+++ led.v
@@ -10,11 +10,37 @@ assign oLED = led;
 
 freq_div UFD (.iCLK(iCLK), .iRESETn(iRESETn), .oMYCLK(MYCLK));
 
+//S0 ready, S1 up, S2 down
+reg [1:0] curr_state, next_state;
+localparam S0 = 2'b00, S1 = 2'b01, S2 = 2'b10;
+
+
+always @(posedge MYCLK) begin
+	case(curr_state)
+		S0 :
+			led = 0;
+		S1 :
+			led = led + 8'd1;
+		S2 :
+			led = led - 8'd1;
+		default : led = led;
+	endcase
+end
+
 always @(posedge MYCLK or negedge iRESETn) begin
-	if(!iRESETn)
-		led <= 0;
+	if(!iRESETn) 
+		curr_state <= S0;
 	else
-		led <= led + 1;
+		curr_state <= next_state;
+end
+
+always @(iUP or iDOWN) begin
+		if(!iUP)
+			next_state = S1;
+		else if(!iDOWN)
+			next_state = S2;
+		else
+			next_state = curr_state;
 end
 
 endmodule
